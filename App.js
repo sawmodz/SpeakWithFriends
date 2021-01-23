d03b386ea7a6ece7c80d37c775a5811cc1080d08
Theo <theowincke@live.fr>
2021-01-23 02:49:37 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.bold, styles.color, {fontSize:40}]}>Je n'aime pas BOW</Text>
+        <StatusBar style="auto"/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bold:{
+    fontWeight:'bold',
+  },
+  colorRed:{
+    color:'red',
+  }
+})
